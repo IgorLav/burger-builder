5f303a58665637486765b2d88dc2819e0142d6ff
IgorLav <i.lavskij@gmail.com>
2017-12-02 04:09:24 +0200

    pass ingredients like query params from burger builder to checkout page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,12 +2,17 @@ import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 export default class Checkout extends Component {
-    state= {
-        ingredients: {
-            salad: 1,
-            bacon: 1
-        }
+    state = {
+        ingredients: {}
     };
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()){
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients});
+    }
 
     checkoutCanceled = () => {
         this.props.history.goBack();
@@ -18,6 +23,7 @@ export default class Checkout extends Component {
     };
 
     render() {
+        console.log(this.state.ingredients);
         return (
             <div className="">
                 <CheckoutSummary ingredients={this.state.ingredients}
