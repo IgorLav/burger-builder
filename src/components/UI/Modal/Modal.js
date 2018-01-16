9ecc296941ecc99b2fd0b2b351591cb385b3c86f
IgorLav <i.lavskij@gmail.com>
2018-01-16 11:18:10 +0200

    Change modal animation

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,19 +3,23 @@ import classes from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop'
 
 class Modal extends Component {
-    shouldComponentUpdate (nextProps, nextState) {
+    shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show || this.props.children !== nextProps.children
     }
 
     render() {
         const {children, show, close} = this.props;
+        const styles = show ? {
+            transform: 'translate(-50%, -50%) scale(1)',
+            opacity: 1
+        } : {
+            transform: 'translate(-50%, -50%) scale(.6)',
+        };
 
         return (
             <React.Fragment>
                 <Backdrop show={show} close={close}/>
-                <div className={classes.Modal}
-                     style={{transform: show ? 'translateY(0)' : 'translateY(-100vh)'}}
-                >
+                <div className={classes.Modal} style={styles}>
                     {children}
                 </div>
             </React.Fragment>
